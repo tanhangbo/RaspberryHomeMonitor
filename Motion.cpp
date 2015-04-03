57eb9faa1c26e995bcf49786b9af73f6a7414bfd
tanhangbo <tan@ubuntu.ubuntu-domain>
2015-04-03 23:03:41 +0800

    add fifo to be a more flexible structure

--- Motion.cpp
+++ Motion.cpp
@@ -8,37 +8,23 @@
 
 #include "global.h"
 
+#include <queue>
+using namespace std;  
+
 
 #define KEY_ESC 27
 #define DELAY_TIME 30
 #define ERODE_TIMES 1
 #define CAMERA_INDEX 1
+#define SHOW_IMAGE_WINDOW 1
+
+queue<IplImage *> webcam_buf;
 
 
-bool have_event = false;
 
-
-std::mutex frame_lock;
-
-IplImage *frame;
-IplImage *frame1;
-IplImage *frame2;
-IplImage *frame3;
-IplImage *frame_gray1;
-IplImage *frame_gray2;
-IplImage *frame_gray3;
-IplImage *frame_result1;
 
 bool send_one_image = false;
 bool send_one_video = false;
-
-
-
-//note:#define cvCaptureFromCAM cvCreateCameraCapture
-
-
-
-
 
 void pop3_handlee(int remote_command)
 {
@@ -52,19 +38,6 @@ void pop3_handlee(int remote_command)
 }
 
 
-void* camera_present_thread(void *arg)
-{
-	while (true) {
-		/*CvCapture *capture = cvCreateCameraCapture(CAMERA_INDEX);
-		if (NULL == capture)
-			printf("unplugd!!!\n");
-		else
-			cvReleaseCapture(&capture); 
-		*/
-		sleep(1);
-	}
-}
-
 
 void* pop3_thread(void *arg)
 {
@@ -72,13 +45,14 @@ void* pop3_thread(void *arg)
 }
 
 
-//http://www.thegeekstuff.com/2012/04/create-threads-in-linux/
-//todo : need free resource
+
+
+/*
 void* play_sound(void *arg)
 {
 	while (true) {
 		if (have_event) {
-			system("aplay ALARM8.WAV");
+			system("aplay BUZZ4.WAV");
 			sleep(65);
 		}
 		sleep(1);//1 second
@@ -86,45 +60,37 @@ void* play_sound(void *arg)
 	return NULL;
 }
 
-
-
-//buffer disable have_event
-void* event_thread(void *arg)
-{
-	while (true) {
-		if (have_event) {
-			frame_lock.lock();
-			if(!cvSaveImage("XIMAGE.jpg", frame))
-				printf("Could not save: %s\n","XIMAGE.jpg");			
-			frame_lock.unlock();
-			smtp_entry("./XIMAGE.jpg");
-
-			sleep(60);//1 minute
-			have_event = false;
-		}
-		if (send_one_image) {
-			send_one_image = false;
-			frame_lock.lock();
-			if(!cvSaveImage("IMAGE.jpg", frame))
-				printf("Could not save: %s\n","IMAGE.jpg");			
-			frame_lock.unlock();
-			smtp_entry("./IMAGE.jpg");
-		}
-		sleep(1);//1 second
-	}
-
-	return NULL;
-}
+*/
 
 
 
-bool detect_motion()
+
+
+IplImage *frame_gray1 = NULL;
+IplImage *frame_gray2 = NULL;
+IplImage *frame_gray3 = NULL;
+IplImage *frame_result1 = NULL;
+std::mutex frame_lock;
+
+
+enum cur_state {
+
+	NORMAL,
+	DETECTED,
+	RECORDING,
+	END_REC,
+
+};
+
+//todo:1.一开机就会检测到的fix 2.检查内存泄露
+//using cvsaveimage to see the first 3 pictures
+
+	//cvCopy( const CvArr* src, CvArr* dst
+bool detect_motion(IplImage *frame1, IplImage *frame2, IplImage *frame3)
 {
 	int black_pixels = 0;
 
-	cvCopy(frame2, frame1);
-	cvCopy(frame3, frame2);
-	cvCopy(frame, frame3);
+
 
 	//Failed to load OpenCL runtime 
 	cvCvtColor(frame1, frame_gray1, CV_BGR2GRAY);
@@ -139,17 +105,175 @@ bool detect_motion()
         cvThreshold(frame_result1, frame_result1, 35, 255, CV_THRESH_BINARY);
 
 
+
+
 	//http://blog.csdn.net/futurewu/article/details/10047409
 	//消除较小独点如噪音
 	cvErode(frame_result1, frame_result1, 0, ERODE_TIMES);
 		
 	black_pixels = cvCountNonZero(frame_result1);
+	//printf(">%d\n", black_pixels);
 	if (black_pixels > 1000) {
 		printf("MOTION_%d!\n", black_pixels);
 		return true;
 	}
 	return false;
 }
+
+
+CvVideoWriter *create_video(CvVideoWriter **video_writer, IplImage *frame)
+{
+
+	time_t current_time;
+	struct tm * time_info;
+	char timeString[50] = {0};
+	time(&current_time);
+	time_info = localtime(&current_time);
+	strftime(timeString, 50, "%m%d_%H%M%S.avi", time_info);
+	printf("creating video:%s\n", timeString);
+	*video_writer =  cvCreateVideoWriter(timeString,
+				CV_FOURCC('M', 'J', 'P', 'G'), 25.0, cvGetSize(frame));
+
+}
+
+
+
+
+void* dispatch_thread(void *arg)
+{
+
+
+
+
+	IplImage *frame = NULL;
+
+	IplImage *frame1 = NULL;
+	IplImage *frame2 = NULL;
+	IplImage *frame3 = NULL;
+	CvVideoWriter *video_writer = NULL;
+
+	int video_count = 0;
+
+	int init_step = 0;
+
+	cur_state state = NORMAL;
+
+	while (true) {
+
+		//printf("size=%d\n", webcam_buf.size());
+		frame_lock.lock();
+		if(!webcam_buf.empty()) {
+			frame = webcam_buf.front();
+			webcam_buf.pop();
+		}
+		frame_lock.unlock();
+
+		if (NULL == frame) {
+			usleep(1000);
+			continue;
+		}
+
+
+		//void cvCopy( const CvArr* src, CvArr* dst, const CvArr* mask=NULL );
+		if (init_step < 3) {
+			if (0 == init_step) {
+				frame1 =  cvCreateImage(cvGetSize(frame),frame->depth,3);
+				frame2 =  cvCreateImage(cvGetSize(frame),frame->depth,3);
+				frame3 =  cvCreateImage(cvGetSize(frame),frame->depth,3);
+				frame_gray1 = cvCreateImage(cvGetSize(frame),frame->depth,1);
+				frame_gray2 = cvCreateImage(cvGetSize(frame),frame->depth,1);
+				frame_gray3 = cvCreateImage(cvGetSize(frame),frame->depth,1);
+				frame_result1 = cvCreateImage(cvGetSize(frame),frame->depth,1);	
+
+				cvCopy(frame, frame1);
+			} else if (1 == init_step) {
+				cvCopy(frame, frame2);
+			} else if (2 == init_step) {
+				cvCopy(frame, frame3);
+			}
+
+			init_step++;
+		} else {
+
+			//cur_frame -> frame3 -> frame2 -> frame1
+			cvCopy(frame2, frame1);
+			cvCopy(frame3, frame2);
+			cvCopy(frame, frame3);
+		}
+
+
+
+		switch(state) {
+
+		case NORMAL:
+			if(detect_motion(frame1, frame2, frame3))
+				state = DETECTED;
+
+
+			if (send_one_image) {
+				send_one_image = false;
+				if(!cvSaveImage("IMAGE.jpg", frame))
+					printf("Could not save: %s\n","IMAGE.jpg");
+				else
+					printf("Saving: %s\n","IMAGE.jpg");
+				smtp_entry("./IMAGE.jpg");
+			}
+
+		break;
+
+		case DETECTED:
+			printf("detected!!!\n");
+
+
+			//1. get current picture and send
+			if(!cvSaveImage("XIMAGE.jpg", frame))
+				printf("Could not save: %s\n","XIMAGE.jpg");
+			else
+				printf("Saving: %s\n","XIMAGE.jpg");
+
+			smtp_entry("./XIMAGE.jpg");
+
+			create_video(&video_writer, frame);
+
+
+			state = RECORDING;
+		break;
+
+		case RECORDING:
+			//2. record 1 minute video then stop and rejudge and recode
+			if (video_count < 1000) {
+				cvWriteFrame(video_writer, frame);
+				video_count++;
+			} else {
+				video_count = 0;
+				state = END_REC;
+			}
+		break;
+
+		case END_REC:
+			if (video_writer) {
+				printf("stop record\n");
+				cvReleaseVideoWriter(&video_writer);
+			}
+
+			state = NORMAL;
+		break;
+
+		}
+
+
+
+
+		//free the frame
+		cvReleaseImage(&frame);
+		frame = NULL;
+
+	}
+
+
+	return NULL;
+}
+
 
 /*
 
@@ -163,109 +287,48 @@ int main(int argc,char **argv)
 	int err = 0;
 	pthread_t tid[10];
 
-
+#if SHOW_IMAGE_WINDOW
 	cvNamedWindow("camera_player", CV_WINDOW_AUTOSIZE);
+#endif
+
+	IplImage *frame = NULL;
 	CvCapture *capture = cvCreateCameraCapture(CAMERA_INDEX);
 	assert(NULL != capture);
 
-
-	frame = cvQueryFrame(capture);
 	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, 640);
 	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 480);
 
-	frame = cvQueryFrame(capture);
-	frame1 =  cvCreateImage(cvGetSize(frame),frame->depth,3);
-	frame2 =  cvCreateImage(cvGetSize(frame),frame->depth,3);
-	frame3 =  cvCreateImage(cvGetSize(frame),frame->depth,3);
-
-	frame_gray1 = cvCreateImage(cvGetSize(frame),frame->depth,1);
-	frame_gray2 = cvCreateImage(cvGetSize(frame),frame->depth,1);
-	frame_gray3 = cvCreateImage(cvGetSize(frame),frame->depth,1);
-	frame_result1 = cvCreateImage(cvGetSize(frame),frame->depth,1);
-
-	CvVideoWriter *video_writer = NULL;
-
-	frame = cvQueryFrame(capture);
-	cvCopy(frame, frame1);
-	frame = cvQueryFrame(capture);
-	cvCopy(frame, frame2);
-	frame = cvQueryFrame(capture);
-	cvCopy(frame, frame3);
 
 
-	err = pthread_create(&(tid[0]), NULL, &play_sound, NULL);
+	err = pthread_create(&(tid[4]), NULL, &pop3_thread, NULL);
 	if (err != 0)
 		printf("\ncan't create thread :[%s]", strerror(err));
-	else
-		printf("\n Thread created successfully\n");
 
-	err = pthread_create(&(tid[1]), NULL, &event_thread, NULL);
+	err = pthread_create(&(tid[5]), NULL, &dispatch_thread, NULL);
 	if (err != 0)
 		printf("\ncan't create thread :[%s]", strerror(err));
-	else
-		printf("\n Thread created successfully\n");
-
-	err = pthread_create(&(tid[2]), NULL, &pop3_thread, NULL);
-	if (err != 0)
-		printf("\ncan't create thread :[%s]", strerror(err));
-	else
-		printf("\n Thread created successfully\n");
-
-	err = pthread_create(&(tid[3]), NULL, &camera_present_thread, NULL);
-	if (err != 0)
-		printf("\ncan't create thread :[%s]", strerror(err));
-	else
-		printf("\n Thread created successfully\n");
-
-
 
 
 	while (true) {
-		frame_lock.lock();
+		
 		frame = cvQueryFrame(capture);
-		frame_lock.unlock();
 
 		if (!frame) {
 			printf("\n critical error when capture\n");
 			break;
 		}
 
+		/* enqueue a frame, must be copied before enqueued */
+		frame_lock.lock();
+		IplImage *frame_copy = cvCreateImage(cvGetSize(frame),frame->depth,3);
+		cvCopy(frame, frame_copy);
+		webcam_buf.push(frame_copy);
+		frame_lock.unlock();
 
-		if (!have_event) {
-			/* if have_event , detect 60s later */
-			have_event = detect_motion();
-		}
+#if SHOW_IMAGE_WINDOW
+		cvShowImage("camera_player", frame);
+#endif
 
-
-
-		//todo :get it into a thread to make it record
-		//every 1minute to avoid attachment too big
-		/* write video */
-		if (have_event) {
-			if(NULL == video_writer) {
-
-				time_t current_time;
-				struct tm * time_info;
-				char timeString[50] = {0};
-				time(&current_time);
-				time_info = localtime(&current_time);
-				strftime(timeString, 50, "%m%d_%H%M%S.avi", time_info);
-				printf("creating video:%s\n", timeString);
-				video_writer =  cvCreateVideoWriter(timeString,
-					CV_FOURCC('M', 'J', 'P', 'G'), 25.0, cvSize(640, 480));
-			}
-			cvWriteFrame(video_writer, frame);
-		} else {
-			if (video_writer)
-				cvReleaseVideoWriter(&video_writer);
-		}
-
-
-
-		cvShowImage("camera_player",frame);
-
-		//usleep(1000*DELAY_TIME);
-		//must need it to display
 		char c = cvWaitKey(DELAY_TIME);
 		if (KEY_ESC == c)
 			break;
@@ -273,8 +336,9 @@ int main(int argc,char **argv)
 	}
 
 	//do not forget to release the resource
-
 	cvReleaseCapture(&capture);
+#if SHOW_IMAGE_WINDOW
 	cvDestroyWindow("camera_player");
+#endif
 	return 0;
 }
